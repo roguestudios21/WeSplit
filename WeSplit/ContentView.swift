91da743c38b0eb86c9970839219df8e13655ef3b
roguestudios21 <atharv.choughule@gmail.com>
2025-07-17 17:27:15 +0530

    first coomit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0, 5, 10 , 15, 20]
+    
+    var totalPerPerson: Double {
+        return checkAmount / Double(numberOfPeople + 2) * (1 + Double(tipPercentage)/100)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("number of People", selection: $numberOfPeople){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                    
+                    Section("how much do you want to tip?") {
+                        Picker("Tip Percentage", selection: $tipPercentage){
+                            ForEach(tipPercentages, id: \.self) {
+                                Text(($0), format: .percent )
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused{
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
